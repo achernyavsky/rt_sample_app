3195f3e8d29e48f8a1291211c71dca21470955c6
Alex Chernyavsky <alexander.chernyavsky@gmail.com>
2010-09-15 21:02:44 -0700

    Chapter 8 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -127,10 +127,9 @@ describe User do
         matching_user = User.authenticate(@attr[:email], @attr[:password])
         matching_user.should == @user
       end
+      
     end
     
   end  
-  
-  
   
 end
